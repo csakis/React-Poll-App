faf2968c82f63c3171f79a1e69d154e7903f98eb
Csaba Kiss <csakis@gmail.com>
2016-01-06 17:23:33 -0700

    header h2

--- components/parts/Header.js
+++ components/parts/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
 			<header className="row">
 				<div className="col-xs-10">
 					<h1>{this.props.title}</h1>
-					<h3>{this.props.speaker}</h3>
+					<h2>{this.props.speaker}</h2>
 				</div>
 				<div className="col-xs-2">
 					<span id="connection-status" className={this.props.status}></span>
